f55018132e334b0b1ade42595cc9faad338936c3
anzhi <191036020@qq.com>
2017-12-04 21:11:13 +0800

    qr code scan

---
+++ veenoon/Engineer/EngineerShootQRCode.h
@@ -0,0 +1,23 @@
+//
+//  ShootQRCode.h
+//  Hint
+//
+//  Created by jack on 12/14/15.
+//  Copyright (c) 2015 jack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EngineerShootQRCode : NSObject
+{
+    
+}
+@property (nonatomic, weak) UIViewController *_viewController;
+
+
++ (EngineerShootQRCode*)sharedShootCodeInstance;
+- (void) shootCode:(UIViewController*)sender;
+
+- (void) showMyCode:(UIViewController*)sender;
+
+@end
