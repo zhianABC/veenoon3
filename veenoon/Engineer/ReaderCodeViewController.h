6e431b0448a8f16662b836cb323fcfe7a6914800
anzhi <191036020@qq.com>
2017-12-04 21:19:51 +0800

    qr code

---
+++ veenoon/Engineer/ReaderCodeViewController.h
@@ -0,0 +1,27 @@
+//
+//  ReaderCodeViewController.h
+//  Hint
+//
+//  Created by chen jack on 2017/3/29.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@protocol ReaderCodeDelegate <NSObject>
+
+@optional
+- (void) didReaderBarData:(NSString*)barString;
+
+@end
+
+@interface ReaderCodeViewController : UIViewController
+{
+    
+}
+@property (nonatomic, weak) id <ReaderCodeDelegate> delegate;
+@property (nonatomic, assign) int postion;
+
+- (void) setDevicePosition:(int)pos;
+
+@end
