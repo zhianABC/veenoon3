c0c7985fc0a1258fe941dc241a0de9c7025a2508
anzhi <191036020@qq.com>
2017-12-17 16:18:36 +0800

    sss

--- veenoon/Engineer/EngineerWirlessYaoBaoViewCtrl.m
+++ veenoon/Engineer/EngineerWirlessYaoBaoViewCtrl.m
@@ -10,6 +10,10 @@
 #import "UIButton+Color.h"
 #import "CustomPickerView.h"
 #import "EngineerSliderView.h"
+#import "SlideButton.h"
+#import "BatteryView.h"
+#import "SignalView.h"
+
 
 @interface EngineerWirlessYaoBaoViewCtrl () {
     
@@ -29,14 +33,100 @@
     UIButton *_tanchuBtn;
     
     EngineerSliderView *_zengyiSlider;
+    
+    NSMutableArray *_imageViewArray;
+    NSMutableArray *_buttonArray;
 }
 @end
 
 @implementation EngineerWirlessYaoBaoViewCtrl
 @synthesize _wirelessYaoBaoSysArray;
 @synthesize _number;
+- (void) inintData {
+    if (_wirelessYaoBaoSysArray) {
+        [_wirelessYaoBaoSysArray removeAllObjects];
+    } else {
+        _wirelessYaoBaoSysArray = [[NSMutableArray alloc] init];
+    }
+    NSMutableDictionary *wuxianDic1 = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"name", @"huangliurong"
+                                       @"status", @"off"
+                                       @"singnal", @"1"
+                                       @"type", @"huatong"
+                                       @"dianliang", @"100", nil];
+    NSMutableDictionary *wuxianDic2 = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"name", @"huangliurong"
+                                       @"status", @"off"
+                                       @"singnal", @"3"
+                                       @"type", @"yaobao"
+                                       @"dianliang", @"60", nil];
+    NSMutableDictionary *wuxianDic3 = [NSMutableDictionary dictionaryWithObjectsAndKeys:@"name", @"huangliurong"
+                                       @"status", @"off"
+                                       @"singnal", @"5"
+                                       @"type", @"huatong"
+                                       @"dianliang", @"60", nil];
+    
+    NSMutableArray *array1 = [NSMutableArray arrayWithObjects:wuxianDic1, wuxianDic2, wuxianDic3, nil];
+    NSMutableDictionary *dic1 = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                                 @"001", @"name",
+                                 array1, @"value", nil];
+    [_wirelessYaoBaoSysArray addObject:dic1];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _imageViewArray = [[NSMutableArray alloc] init];
+    _buttonArray = [[NSMutableArray alloc] init];
+    
+    [self inintData];
+    
+    UIImageView *titleIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"main_view_title.png"]];
+    [self.view addSubview:titleIcon];
+    titleIcon.frame = CGRectMake(70, 30, 70, 10);
+    
+    UILabel *line = [[UILabel alloc] initWithFrame:CGRectMake(0, 59, SCREEN_WIDTH, 1)];
+    line.backgroundColor = RGB(75, 163, 202);
+    [self.view addSubview:line];
+    
+    UIImageView *bottomBar = [[UIImageView alloc] initWithFrame:CGRectMake(0, SCREEN_HEIGHT-60, SCREEN_WIDTH, 60)];
+    [self.view addSubview:bottomBar];
+    
+    //缺切图，把切图贴上即可。
+    bottomBar.backgroundColor = [UIColor grayColor];
+    bottomBar.userInteractionEnabled = YES;
+    bottomBar.image = [UIImage imageNamed:@"botomo_icon.png"];
+    
+    UIButton *cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    cancelBtn.frame = CGRectMake(10, 0,160, 60);
+    [bottomBar addSubview:cancelBtn];
+    [cancelBtn setTitle:@"返回" forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:RGB(255, 180, 0) forState:UIControlStateHighlighted];
+    cancelBtn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [cancelBtn addTarget:self
+                  action:@selector(cancelAction:)
+        forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    okBtn.frame = CGRectMake(SCREEN_WIDTH-10-160, 0,160, 60);
+    [bottomBar addSubview:okBtn];
+    [okBtn setTitle:@"设置" forState:UIControlStateNormal];
+    [okBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [okBtn setTitleColor:RGB(255, 180, 0) forState:UIControlStateHighlighted];
+    okBtn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [okBtn addTarget:self
+              action:@selector(okAction:)
+    forControlEvents:UIControlEventTouchUpInside];
+    
+    _selectSysBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _selectSysBtn.frame = CGRectMake(70, 100, 250, 30);
+    [_selectSysBtn setImage:[UIImage imageNamed:@"engineer_sys_select_down_n.png"] forState:UIControlStateNormal];
+    [_selectSysBtn setTitle:@"无线手持腰包系统" forState:UIControlStateNormal];
+    [_selectSysBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_selectSysBtn setTitleColor:RGB(230, 151, 50) forState:UIControlStateHighlighted];
+    _selectSysBtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentCenter;
+    [_selectSysBtn setTitleEdgeInsets:UIEdgeInsetsMake(0,-50,0,_selectSysBtn.imageView.bounds.size.width+50)];
+    [_selectSysBtn setImageEdgeInsets:UIEdgeInsetsMake(0,_selectSysBtn.titleLabel.bounds.size.width,0,-130)];
+    [_selectSysBtn addTarget:self action:@selector(sysSelectAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:_selectSysBtn];
     
     
     _zengyiSlider = [[EngineerSliderView alloc]
@@ -50,7 +140,191 @@
     _zengyiSlider.maxValue = 20;
     _zengyiSlider.minValue = -20;
     [_zengyiSlider resetScale];
-    _zengyiSlider.center = CGPointMake(600, 500);
+    _zengyiSlider.center = CGPointMake(SCREEN_WIDTH - 150, SCREEN_HEIGHT/2);
+    
+    int index = 0;
+    int top = 250;
+    if (self._number == 8) {
+        top = 350;
+    }
+    
+    int leftRight = 100;
+    
+    int cellWidth = 92;
+    int cellHeight = 92;
+    int colNumber = 8;
+    int space = (SCREEN_WIDTH - colNumber*cellWidth-leftRight*2)/(colNumber-1);
+    
+    NSMutableDictionary *dataDic = [_wirelessYaoBaoSysArray objectAtIndex:0];
+    NSMutableArray *dataArray = [dataDic objectForKey:@"value"];
+    
+    if ([dataArray count] == 0) {
+        int nameStart = 1;
+        for (int i = 0; i < self._number; i++) {
+            
+        }
+    } else {
+        for (int i = 0; i < [dataArray count]; i++) {
+            NSMutableDictionary *dataDic = [dataArray objectAtIndex:i];
+            
+            int row = index/colNumber;
+            int col = index%colNumber;
+            int startX = col*cellWidth+col*space+leftRight;
+            int startY = row*cellHeight+space*row+top;
+            
+            SlideButton *btn = [[SlideButton alloc] initWithFrame:CGRectMake(startX, startY, 120, 120)];
+            btn.tag = i;
+            [self.view addSubview:btn];
+            
+            UILabel* titleL = [[UILabel alloc] initWithFrame:CGRectMake(btn.frame.size.width - 20, 0, 20, 20)];
+            titleL.backgroundColor = [UIColor clearColor];
+            [btn addSubview:titleL];
+            titleL.font = [UIFont boldSystemFontOfSize:11];
+            titleL.textColor  = [UIColor whiteColor];
+            titleL.text = [dataDic objectForKey:@"name"];
+            
+            titleL = [[UILabel alloc] initWithFrame:CGRectMake(btn.frame.size.width/2 -40, btn.frame.size.height - 20, 80, 20)];
+            titleL.backgroundColor = [UIColor clearColor];
+            [btn addSubview:titleL];
+            titleL.font = [UIFont boldSystemFontOfSize:12];
+            titleL.textColor  = [UIColor whiteColor];
+            titleL.textAlignment = NSTextAlignmentCenter;
+            titleL.text = @"Channel";
+            
+            UILongPressGestureRecognizer *longPress2 = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressed2:)];
+            
+            [btn addGestureRecognizer:longPress2];
+            
+            
+            UIImage *image;
+            NSString *huatongType = [dataDic objectForKey:@"type"];
+            if ([@"huatong" isEqualToString:huatongType]) {
+                image = [UIImage imageNamed:@"wuxianhuatong.png"];
+            } else {
+                image = [UIImage imageNamed:@"wuxianhuabao.png"];
+            }
+            
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+            imageView.frame = CGRectMake(btn.frame.origin.x, btn.frame.origin.y, btn.frame.size.width, btn.frame.size.height);
+            imageView.tag = index;
+            imageView.userInteractionEnabled=YES;
+            imageView.layer.contentsGravity = kCAGravityCenter;
+            [self.view addSubview:imageView];
+            
+            BatteryView *batter = [[BatteryView alloc] initWithFrame:CGRectZero];
+            batter.normalColor = SINGAL_COLOR;
+            [imageView addSubview:batter];
+            batter.center = CGPointMake(60, 18);
+            
+            NSString *dianliangStr = [dataDic objectForKey:@"dianliang"];
+            int dianliang = [dianliangStr intValue];
+            double dianliangDouble = 1.0f * dianliang / 100;
+            [batter setBatteryValue:dianliangDouble];
+            
+            SignalView *signal = [[SignalView alloc] initWithFrameAndStep:CGRectMake(70, 50, 30, 20) step:2];
+            [imageView addSubview:signal];
+            [signal setLightColor:SINGAL_COLOR];//SINGAL_COLOR
+            [signal setGrayColor:[UIColor colorWithWhite:1.0 alpha:0.6]];
+            NSString *sinalString = [dataDic objectForKey:@"signal"];
+            int signalInt = [sinalString intValue];
+            [signal setSignalValue:signalInt];
+            
+            titleL = [[UILabel alloc] initWithFrame:CGRectMake(0, btn.frame.size.width-30, btn.frame.size.width, 20)];
+            titleL.backgroundColor = [UIColor clearColor];
+            [imageView addSubview:titleL];
+            titleL.font = [UIFont boldSystemFontOfSize:12];
+            titleL.textAlignment = NSTextAlignmentCenter;
+            titleL.textColor  = SINGAL_COLOR;
+            titleL.text = [dataDic objectForKey:@"name"];
+            imageView.hidden = YES;
+            
+            UILongPressGestureRecognizer *longPress3 = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressed3:)];
+            [imageView addGestureRecognizer:longPress3];
+            
+            [_imageViewArray addObject:imageView];
+            [_buttonArray addObject:btn];
+            
+            index++;
+        }
+    }
 }
 
+- (void) longPressed3:(id)sender{
+    
+    UILongPressGestureRecognizer *press = (UILongPressGestureRecognizer *)sender;
+    if (press.state == UIGestureRecognizerStateEnded) {
+        // no need anything here
+        return;
+    } else if (press.state == UIGestureRecognizerStateBegan) {
+        int index = (int) press.view.tag;
+        UIButton *button = [_buttonArray objectAtIndex:index];
+        UIImageView *imageView = [_imageViewArray objectAtIndex:index];
+        if (button.isHidden) {
+            button.hidden = NO;
+            imageView.hidden = YES;
+        } else {
+            button.hidden = YES;
+            imageView.hidden = NO;
+        }
+    }
+}
+
+- (void) longPressed2:(id)sender{
+    
+    UILongPressGestureRecognizer *press = (UILongPressGestureRecognizer *)sender;
+    if (press.state == UIGestureRecognizerStateEnded) {
+        // no need anything here
+        return;
+    } else if (press.state == UIGestureRecognizerStateBegan) {
+        int index = (int) press.view.tag;
+        UIButton *button = [_buttonArray objectAtIndex:index];
+        UIImageView *imageView = [_imageViewArray objectAtIndex:index];
+        if (button.isHidden) {
+            button.hidden = NO;
+            imageView.hidden = YES;
+        } else {
+            button.hidden = YES;
+            imageView.hidden = NO;
+        }
+    }
+}
+- (void) scenarioAction:(id)sender{
+    
+}
+
+- (void) sysSelectAction:(id)sender{
+    _customPicker = [[CustomPickerView alloc]
+                     initWithFrame:CGRectMake(_selectSysBtn.frame.origin.x, _selectSysBtn.frame.origin.y, _selectSysBtn.frame.size.width, 200) withGrayOrLight:@"gray"];
+    
+    
+    NSMutableArray *arr = [NSMutableArray array];
+    for(int i = 1; i< 2; i++)
+    {
+        [arr addObject:[NSString stringWithFormat:@"00%d", i]];
+    }
+    
+    _customPicker._pickerDataArray = @[@{@"values":arr}];
+    
+    
+    _customPicker._selectColor = [UIColor orangeColor];
+    _customPicker._rowNormalColor = [UIColor whiteColor];
+    [self.view addSubview:_customPicker];
+    _customPicker.delegate_ = self;
+}
+
+- (void) didConfirmPickerValue:(NSString*) pickerValue {
+    if (_customPicker) {
+        [_customPicker removeFromSuperview];
+    }
+    NSString *title =  [@"无线手持腰包系统" stringByAppendingString:pickerValue];
+    [_selectSysBtn setTitle:title forState:UIControlStateNormal];
+}
+
+- (void) okAction:(id)sender{
+    
+}
+
+- (void) cancelAction:(id)sender{
+    [self.navigationController popViewControllerAnimated:YES];
+}
 @end
