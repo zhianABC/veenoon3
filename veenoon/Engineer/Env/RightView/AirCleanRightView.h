891e43cc4cb3e2d7ab753c0c3cb19ab922d57743
anzhi <191036020@qq.com>
2018-02-24 10:26:52 +0800

    Air clean right view

---
+++ veenoon/Engineer/Env/RightView/AirCleanRightView.h
@@ -0,0 +1,13 @@
+//
+//  AirCleanRightView.h
+//  veenoon
+//
+//  Created by 安志良 on 2018/2/24.
+//  Copyright © 2018年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AirCleanRightView : UIView
+
+@end
