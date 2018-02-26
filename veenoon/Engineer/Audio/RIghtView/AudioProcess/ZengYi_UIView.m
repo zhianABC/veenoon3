7facdb4a7f46e1c1e59f4221d06fd809c62ecb28
anzhi <191036020@qq.com>
2018-02-26 21:33:12 +0800

    dd

--- veenoon/Engineer/Audio/RIghtView/AudioProcess/ZengYi_UIView.m
+++ veenoon/Engineer/Audio/RIghtView/AudioProcess/ZengYi_UIView.m
@@ -64,23 +64,33 @@
     
     self._channelBtns = [NSMutableArray array];
     
-    int x = 0;
+    float x = 0;
     int y = CGRectGetHeight(self.frame)-60;
     
-    int spx = (CGRectGetWidth(self.frame) - num*50)/(num-1);
-    if(spx > 16)
-        spx = 16;
-    for(int i = 0; i < num; i++) {
+    float spx = (CGRectGetWidth(self.frame) - num*50.0)/(num-1);
+    if(spx > 10)
+        spx = 10;
+    for(int i = 0; i < num; i++)
+    {
         UIButton *btn = [UIButton buttonWithColor:RGB(0, 89, 118) selColor:nil];
         btn.frame = CGRectMake(x, y, 50, 50);
         btn.clipsToBounds = YES;
         btn.layer.cornerRadius = 5;
         btn.titleLabel.font = [UIFont systemFontOfSize:15];
         [btn setTitle:[NSString stringWithFormat:@"%d", i+1] forState:UIControlStateNormal];
-        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        [btn setTitleColor:YELLOW_COLOR forState:UIControlStateHighlighted];
         btn.tag = i;
         [self addSubview:btn];
+        
+        if(i == 0)
+        {
+            [btn setTitleColor:YELLOW_COLOR forState:UIControlStateNormal];
+            [btn setTitleColor:YELLOW_COLOR forState:UIControlStateHighlighted];
+        }
+        else
+        {
+            [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+            [btn setTitleColor:YELLOW_COLOR forState:UIControlStateHighlighted];
+        }
         
         [btn addTarget:self
                 action:@selector(channelBtnAction:)
