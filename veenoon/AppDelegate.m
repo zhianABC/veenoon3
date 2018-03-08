7fdb6ee4cd7e4c7dcb95d829133ea189364fc671
anzhi <191036020@qq.com>
2018-03-08 20:14:32 +0800

    fix

--- veenoon/AppDelegate.m
+++ veenoon/AppDelegate.m
@@ -11,7 +11,7 @@
 #import "CMNavigationController.h"
 #import "WellcomeViewController.h"
 #import "HomeViewController.h"
-#import "WellcomeViewController.h"
+#import "EngineerPresetScenarioViewCtrl.h"
 
 @interface AppDelegate ()
 {
@@ -30,7 +30,7 @@
     [session setActive:YES error:nil];
     [session setCategory:AVAudioSessionCategoryPlayback error:nil];
     
-     WellcomeViewController *wellcome = [[WellcomeViewController alloc] init];
+     EngineerPresetScenarioViewCtrl *wellcome = [[EngineerPresetScenarioViewCtrl alloc] init];
     _naviRoot = [[CMNavigationController alloc] initWithRootViewController:wellcome];
     _naviRoot.navigationBarHidden = YES;
     self.window.rootViewController = _naviRoot;
