88e84a1959325929b3ce9f032a51c78ee4d01abf
chenjun <13115204@qq.com>
2018-05-24 20:06:47 +0800

    new

---
+++ veenoon/Engineer/Basic/EngineerNewTeslariViewCtrl.h
@@ -0,0 +1,16 @@
+//
+//  EngineerNewTeslariViewCtrl.h
+//  veenoon
+//
+//  Created by 安志良 on 2017/12/7.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BaseViewController.h"
+
+@interface EngineerNewTeslariViewCtrl : UIViewController {
+    NSMutableDictionary *_meetingRoomDic;
+}
+@property (nonatomic,strong) NSMutableDictionary *_meetingRoomDic;
+@end
---
+++ veenoon/Engineer/Basic/EngineerNewTeslariViewCtrl.m
@@ -0,0 +1,111 @@
+//
+//  EngineerNewTeslariViewCtrl.m
+//  veenoon
+//
+//  Created by 安志良 on 2017/12/4.
+//  Copyright © 2017年 jack. All rights reserved.
+//
+
+#import "EngineerNewTeslariViewCtrl.h"
+#import "EngineerAudioDevicePluginViewCtrl.h"
+
+#import "DataSync.h"
+
+
+@interface EngineerNewTeslariViewCtrl () {
+    
+}
+@property (nonatomic, strong) NSArray *_driver_objs;
+@property (nonatomic, strong) NSMutableDictionary *_mapDrivers;
+@property (nonatomic, strong) NSArray *_area_objs;
+
+@end
+
+@implementation EngineerNewTeslariViewCtrl
+@synthesize _meetingRoomDic;
+@synthesize _mapDrivers;
+@synthesize _driver_objs;
+@synthesize _area_objs;
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = BLACK_COLOR;
+    
+    UILabel *portDNSLabel = [[UILabel alloc] initWithFrame:CGRectMake(ENGINEER_VIEW_LEFT, ENGINEER_VIEW_TOP+10, SCREEN_WIDTH-80, 40)];
+    portDNSLabel.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:portDNSLabel];
+    portDNSLabel.font = [UIFont boldSystemFontOfSize:20];
+    portDNSLabel.textColor  = [UIColor whiteColor];
+    portDNSLabel.text = @"欢迎使用TESLARIA";
+    
+    portDNSLabel = [[UILabel alloc] initWithFrame:CGRectMake(ENGINEER_VIEW_LEFT, CGRectGetMaxY(portDNSLabel.frame) + 20, SCREEN_WIDTH-80, 30)];
+    portDNSLabel.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:portDNSLabel];
+    portDNSLabel.font = [UIFont systemFontOfSize:18];
+    portDNSLabel.textColor  = [UIColor colorWithWhite:1.0 alpha:0.9];
+    portDNSLabel.text = @"TESLARIA将引导您完成整个系统的设置过程";
+    
+    portDNSLabel = [[UILabel alloc] initWithFrame:CGRectMake(ENGINEER_VIEW_LEFT, CGRectGetMaxY(portDNSLabel.frame)+5, SCREEN_WIDTH-80, 30)];
+    portDNSLabel.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:portDNSLabel];
+    portDNSLabel.font = [UIFont systemFontOfSize:18];
+    portDNSLabel.textColor  = [UIColor colorWithWhite:1.0 alpha:0.9];
+    portDNSLabel.text = @"音频> 视频> 环境> 其他";
+    
+    UIImageView *bottomBar = [[UIImageView alloc] initWithFrame:CGRectMake(0, SCREEN_HEIGHT-50, SCREEN_WIDTH, 50)];
+    [self.view addSubview:bottomBar];
+    
+    //缺切图，把切图贴上即可。
+    bottomBar.backgroundColor = [UIColor grayColor];
+    bottomBar.userInteractionEnabled = YES;
+    bottomBar.image = [UIImage imageNamed:@"botomo_icon_black.png"];
+    
+    UIButton *cancelBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    cancelBtn.frame = CGRectMake(0, 0, 160, 50);
+    [bottomBar addSubview:cancelBtn];
+    [cancelBtn setTitle:@"返回" forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [cancelBtn setTitleColor:RGB(255, 180, 0) forState:UIControlStateHighlighted];
+    cancelBtn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [cancelBtn addTarget:self
+                  action:@selector(cancelAction:)
+        forControlEvents:UIControlEventTouchUpInside];
+    
+    UIButton *okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    okBtn.frame = CGRectMake(SCREEN_WIDTH-10-160, 0,160, 50);
+    [bottomBar addSubview:okBtn];
+    [okBtn setTitle:@"下一步 >" forState:UIControlStateNormal];
+    [okBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [okBtn setTitleColor:RGB(255, 180, 0) forState:UIControlStateHighlighted];
+    okBtn.titleLabel.font = [UIFont boldSystemFontOfSize:18];
+    [okBtn addTarget:self
+              action:@selector(okAction:)
+    forControlEvents:UIControlEventTouchUpInside];
+    
+#if LOGIN_REGULUS
+    //创建Area
+    [[DataSync sharedDataSync] syncCurrentArea];
+    
+    //获取Regulus支持的插件
+    [[DataSync sharedDataSync] syncRegulusDrivers];
+#endif
+}
+
+- (void) okAction:(id)sender{
+    
+    EngineerAudioDevicePluginViewCtrl *ctrl = [[EngineerAudioDevicePluginViewCtrl alloc] init];
+    ctrl._meetingRoomDic = self._meetingRoomDic;
+    
+    NSMutableDictionary *selectedSysDic = [[NSMutableDictionary alloc] init];
+    ctrl._selectedSysDic = selectedSysDic;
+    
+    [self.navigationController pushViewController:ctrl animated:YES];
+}
+
+- (void) cancelAction:(id)sender{
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
+
