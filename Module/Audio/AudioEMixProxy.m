77d6d1b427ee2c6d2608f658a22b89f31f2e03e3
anzhi <191036020@qq.com>
2018-07-02 21:03:33 +0800

    f

--- Module/Audio/AudioEMixProxy.m
+++ Module/Audio/AudioEMixProxy.m
@@ -100,20 +100,6 @@
     
     
     if(cmds && [cmds count])
-        //set_mode 语音激励，或者是标准发言
-        //set_priority 设置主席/代表
-        //set_mic_open_max 设置最大发言人数
-        //set_vol 设置输出音量
-        //set_mic_delay_close
-        //set_cam_pol 摄像追踪(VISCA)
-        //set_cam_addr 设置摄像地址
-        //set_cam_star_pos 设置预制位
-        //set_peq 设置均衡
-        //set_press 压限
-        //set_noise_gate 噪声们
-        //set_high_filter 高通
-        //set_low_filter 低通
-        //set_fb 反馈抑制(取消)
     {
         self._cmdMap = [NSMutableDictionary dictionary];
         
