6a1565af7699bf11719d9b1da1f6144cd1c48ce9
anzhi <191036020@qq.com>
2018-09-08 17:03:33 +0800

    s

---
+++ Module/AirQualityPlug.h
@@ -0,0 +1,14 @@
+//
+//  AirQualityPlug.h
+//  veenoon
+//
+//  Created by 安志良 on 2018/9/8.
+//  Copyright © 2018年 jack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BasePlugElement.h"
+
+@interface AirQualityPlug : BasePlugElement
+
+@end
---
+++ Module/AirQualityPlug.m
@@ -0,0 +1,13 @@
+//
+//  AirQualityPlug.m
+//  veenoon
+//
+//  Created by 安志良 on 2018/9/8.
+//  Copyright © 2018年 jack. All rights reserved.
+//
+
+#import "AirQualityPlug.h"
+
+@implementation AirQualityPlug
+
+@end
