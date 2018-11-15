e8548bf871d9eaf08dacb7604eddf71aae0c37a7
chen jack <13115204@qq.com>
2018-11-15 16:46:39 +0800

    New plug

--- Module/Audio/AudioEMinMax.m
+++ Module/Audio/AudioEMinMax.m
@@ -7,6 +7,9 @@
 //
 
 #import "AudioEMinMax.h"
+#import "RegulusSDK.h"
+#import "DataSync.h"
+#import "KVNProgress.h"
 
 @implementation AudioEMinMax
 
@@ -20,4 +23,30 @@
     
     return self;
 }
+
+- (void) createDriver{
+    
+    RgsAreaObj *area = [DataSync sharedDataSync]._currentArea;
+    if(area && _driverInfo && !_driver)
+    {
+        RgsDriverInfo *info = _driverInfo;
+        
+        IMP_BLOCK_SELF(AudioEMinMax);
+        
+        [KVNProgress show];
+        [[RegulusSDK sharedRegulusSDK] CreateDriver:area.m_id
+                                             serial:info.serial
+                                         completion:^(BOOL result, RgsDriverObj *driver, NSError *error) {
+                                             if (result) {
+                                                 
+                                                 block_self._driver = driver;
+                                                 block_self._name = driver.name;
+                                                 
+                                                 [[NSNotificationCenter defaultCenter] postNotificationName:@"NotifyRefreshTableWithCom" object:nil];
+                                             }
+                                             [KVNProgress showSuccess];
+                                         }];
+    }
+}
+
 @end
--- Module/Video/VPinJieSet.m
+++ Module/Video/VPinJieSet.m
@@ -7,8 +7,36 @@
 //
 
 #import "VPinJieSet.h"
+#import "RegulusSDK.h"
+#import "DataSync.h"
+#import "KVNProgress.h"
 
 
 @implementation VPinJieSet
+
+- (void) createDriver{
+    
+    RgsAreaObj *area = [DataSync sharedDataSync]._currentArea;
+    if(area && _driverInfo && !_driver)
+    {
+        RgsDriverInfo *info = _driverInfo;
+        
+        IMP_BLOCK_SELF(VPinJieSet);
+        
+        [KVNProgress show];
+        [[RegulusSDK sharedRegulusSDK] CreateDriver:area.m_id
+                                             serial:info.serial
+                                         completion:^(BOOL result, RgsDriverObj *driver, NSError *error) {
+                                             if (result) {
+                                                 
+                                                 block_self._driver = driver;
+                                                 block_self._name = driver.name;
+                                                 
+                                                 [[NSNotificationCenter defaultCenter] postNotificationName:@"NotifyRefreshTableWithCom" object:nil];
+                                             }
+                                             [KVNProgress showSuccess];
+                                         }];
+    }
+}
 
 @end
